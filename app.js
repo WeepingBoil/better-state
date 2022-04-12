5cc077450bdbe4d267f696f70e20583515995599
EJW <git@ejwaller.com>
2022-04-12 14:42:14 +0100

    green

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ class App {
 	#preventBack = 0;
 	#preventForward = 0;
 	#override = false;
+	#listeners = [];
+	#id = 0;
+	#lastPop = null;
+	verbose = false;
 
 	constructor() {
+		if (this.verbose) console.log("better-state");
 		this.#index = isNaN(history.state?.index) ? 0 : history.state.index;
 		this.#history[this.#index] = history.state || {
 			index: this.#index,
@@ -21,7 +26,7 @@ class App {
 			try {
 				this.#history[this.#index].state.scrollX = window.scrollX;
 				this.#history[this.#index].state.scrollY = window.scrollY;
-				history.replaceState(this.#history[this.#index], "", this.#history[this.#index].state.url);
+				history.replaceState(this.#history[this.#index], "", location.pathname);
 			} catch {}
 		});
 		window.addEventListener("popstate", (e) => {
@@ -41,26 +46,22 @@ class App {
 				return;
 			}
 
-			window.dispatchEvent(
-				new CustomEvent("__better-state-pop-state", {
-					detail: {
-						back: this.#prevented === "back" || (this.#prevented !== "forward" && _back === true),
-						forward: this.#prevented === "forward" || (this.#prevented !== "back" && _back === false),
-						prevented: this.#prevented !== false,
-						forced: this.#override === true,
-						state: e.state?.state ?? {}
-					}
-				})
-			);
+			this.#lastPop = {
+				back: this.#prevented === "back" || (this.#prevented !== "forward" && _back === true),
+				forward: this.#prevented === "forward" || (this.#prevented !== "back" && _back === false),
+				prevented: this.#prevented !== false,
+				forced: this.#override === true,
+				state: e.state?.state ?? {},
+				actualState: e
+			};
+
+			for (var listener of this.#listeners) {
+				listener.fn(this.#lastPop);
+			}
 
 			this.#prevented = false;
 			this.#override = false;
 		});
-		try {
-			window.removeEventListener("__better-state-pop-state", this.pop);
-		} catch {}
-
-		window.addEventListener("__better-state-pop-state", this.pop);
 	}
 
 	get preventBack() {
@@ -94,7 +95,11 @@ class App {
 	}
 
 	get state() {
-		return this.#history[this.#index].state;
+		return this.#history[this.#index]?.state ?? null;
+	}
+
+	get lastPop() {
+		return this.#lastPop;
 	}
 
 	forceForward = (internal) => {
@@ -109,19 +114,21 @@ class App {
 
 	replaceState = (url, data) => {
 		this.#history[this.#index].state.url = url;
-		this.#history[this.#index].state.data = data;
-		history.replaceState(this.#history[this.#index], "", this.#history[this.#index].state.url);
+		if (data) this.#history[this.#index].state.data = data;
+		if (this.verbose) console.log("better-state-replace", this.#history[this.#index], url);
+
+		history.replaceState(this.#history[this.#index], "", url);
 		return this.state;
 	};
 
-	pop = (data) => {
-		this.onPopState(data.detail);
+	onPopState = (fn) => {
+		this.#listeners.push({ id: ++this.#id, fn: fn });
+		return this.#id;
 	};
 
-	onPopState = false;
-
-	offPopState = () => {
-		this.onPopState = false;
+	offPopState = (x) => {
+		if (x) this.#listeners.splice(this.#listeners.map((v) => v.id).indexOf(x), 1);
+		else this.#listeners = [];
 	};
 
 	pushState = (url, data) => {
@@ -129,11 +136,12 @@ class App {
 			index: this.#index,
 			state: {
 				url: url,
-				scrollY: window.scrollY,
-				scrollX: window.scrollX,
+				scrollY: 0,
+				scrollX: 0,
 				data: data
 			}
 		};
+		if (this.verbose) console.log("better-state-push", this.#history[this.#index], url);
 		history.pushState(this.#history[this.#index], "", url);
 		return this.state;
 	};
